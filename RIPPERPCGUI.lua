08366fb519fc4e1be2f8c854e5764c6eb5e2852d
hajibeza <74174244+hajibeza@users.noreply.github.com>
2022-08-20 07:50:35 +0700

    Update RIPPERPCGUI.lua

--- RIPPERPCGUI.lua
+++ RIPPERPCGUI.lua
@@ -6732,7 +6732,7 @@ function library:CreateWindow(options, ...)
 		}}, {"AddSlider", "__Designer.Slider.ImageTransparency", backgroundsection, {
 			Name = "Image Transparency",
 			Flag = "__Designer.Background.ImageTransparency",
-			Value = 80,
+			Value = 95,
 			Min = 0,
 			Max = 100,
 			Format = "Image Transparency: %s%%",
